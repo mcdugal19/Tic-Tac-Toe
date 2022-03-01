4b9de2abdc8757db1d367475a06692b271652c4e
Stephen Lance McDaniel <stephen.l.mcdaniel@gmail.com>
2022-03-01 14:43:03 -0600

    End of work day commit: attempted to add eventlisteners to app.js

--- app.js
+++ app.js
@@ -3,51 +3,69 @@
 
 let gameState = {
     players: ['x', 'o'],
-    Xclick: 'X',
-    Oclick: 'O',
+    // Xclick: function click(){'X'
+    //     },
+    // Oclick: function click(){'O'
+    //     },
     board: [
       [null, null, null],
       [null, null, null],
       [null, null, null]
     ],
-
+    player1: 'X' || 'O',
+    player2: 'X' || 'O',
+    playerPC: 'X' || 'O',
+    //scoreStatus: 
     gameStatus: 'playing',
+
     // getRemaining: function(){
     //     return (this.goal - this.currentScore)
     // },
   }
 
 // create board (why doesn't this show up?)
-let gameState.board = document.getElementById("table"),
-let b1 = document.getElementsByTagNameNS('td')[0]
-let b2 = gameState.board[0][1]
-let b3 = gameState.board[0][2]
-let b4 = gameState.board[1][0]
-let b5 = gameState.board[1][1]
-let b6 = gameState.board[1][2]
-let b7 = gameState.board[2][0]
-let b8 = gameState.board[2][1]
-let b9 = gameState.board[2][2]
+// gameState.board[0][0] = document.getElementById("td")
+
+// let table = document.getElementsByTagName('table')
+// let b1 = document.getElementsByTagName('td')[0]
+// let b2 = document.getElementsByTagName('td'[1])
+// let b3 = document.getElementsByTagName('td'[2])
+// let b4 = document.getElementsByTagName('td'[3])
+// let b5 = document.getElementsByTagName('td'[4])
+// let b6 = document.getElementsByTagName('td'[5])
+// let b7 = document.getElementsByTagName('td'[6])
+// let b8 = document.getElementsByTagName('td'[7])
+// let b9 = document.getElementsByTagName('td'[8])
 
 
-console.log(b1)
+
+let Xfill = 'X'
+let b1button = document.getElementById('b1')
+
 function markXorO(){
-    
-    if(players === 'X'){
-    board.appendChild(b1)
-    b1.innerText = 'X'
-    }
-    else{
-        board.appendChild(b1)
-        b1.innerText = 'O'
-    }
+       b1.toggle(Xfill) 
+         
+
 }
 
+b1button.addEventListener("click", markXorO)
 
-button.addEventListener('click', function(){
-    markXorO()
+// console.log(b1, 'Hello')
+function markXorO(){
+    b1.innerText = 'X'    
+    }
+//     else{
+//         board.appendChild(b1)
+//         b1.innerText = 'O'
+//     }
+// }
+
+
+// b1.addEventListener('click', markXorO)
+
     // renderGame()
-})
+
+
 
 
 
@@ -59,10 +77,10 @@ button.addEventListener('click', function(){
 // // = write functions to manipulate gamestate after every click
 //   const board = document.querySelector('.board')
 
-//   const button = document.createElement('button')
-//   button.classList.add()
-//   board.appendChild(button)
-//   button.innerText = "Choose Single or Double Player"
+  const button = document.createElement('button')
+  button.classList.add()
+  board.appendChild(button)
+  button.innerText = "Choose Single or Double Player"
 
 //   console.log(board)
 //     function markX(){
