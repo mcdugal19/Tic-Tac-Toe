004a934763d252914732be014f5eba5326d33092
Stephen Lance McDaniel <stephen.l.mcdaniel@gmail.com>
2022-03-01 22:17:05 -0600

    End of night: Header added to HTML, debugging unsuccesful

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const gameState = {
             [null, null, null],
             [null, null, null],
     ],
-    currentPlayer: 'X',
+    currentPlayer: 'X' || 'O',
+    // finished: clearboardfunc
 }
 
 const grid = document.querySelector('.grid')
 
 grid.addEventListener('click', function (event){
-    const coordinateArray = event.target.dispatchEvent.split(',')
+    const coordinateArray = event.target.id.split(',')
     console.log(coordinateArray)
     const x = coordinateArray[0]
     const y = coordinateArray[1]
@@ -22,6 +23,45 @@ grid.addEventListener('click', function (event){
     }
 })
 
+//**HEADER */
+let h1 = document.querySelector('h1')
+function makeh1(h1){
+h1.innerText('TIC TAC TOE')
+}
+innerText.value = h1.innerText
+console.log(h1)
+
+
+//**SCORE TICKER */
+
+//**BUTTON TO CHOOSE 1P OR 2P? */
+
 //Render Function ==> Loop over the board, use the
 // indexes and values to query the element and 
 // update it in the browser
+
+//update progress
+function update(progress) {
+    gameState.board[x][y] += progress
+
+    if(gameState.board = finished
+    ){
+        gameState.board = finished
+    }
+}
+//alternative below
+// function gameLoop(){
+//     window.requestAnimationFrame(gameLoop);
+//     Game.update();
+//   }
+
+
+function updateScore() {
+    //update score ticker
+}
+
+
+//winning functions
+// check every possibility? no
+// any 3 X in a line?
+// or any 3 O in a line
