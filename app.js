ba1e398525271247a8e3c939c9ffd7d7d13795ce
Stephen Lance McDaniel <stephen.l.mcdaniel@gmail.com>
2022-03-02 10:19:13 -0600

    Added buttons below the header to choose 1P or 2P game

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const gameState = {
             [null, null, null],
             [null, null, null],
     ],
-    currentPlayer: 'X' || 'O',
+    currentPlayer: 'X' || 'O', //WANT To make it random, insert variable
+    // then add a function to randomize the x and o even vs odd
+    // math.floor(math.random()) look up doc
+    singlePlayer: true
+    //pcPlayer: true || false
     // finished: clearboardfunc
 }
 
 const grid = document.querySelector('.grid')
+
+// grid.addEventListener('click', function (event){
+//     const coordinateArray = event.target.id.split(',')
+//     console.log(coordinateArray)
+//     const x = coordinateArray[0]
+//     const y = coordinateArray[1]
+//     gameState.board[x][y] = gameState.currentPlayer
+//     if (gameState.currentPlayer === 'X') {
+//         gameState.currentPlayer = 'O'
+//     } else if (gameState.currentPlayer === 'O'){
+//         gameState.currentPlayer = 'X'
+//     }
+// })
 
 grid.addEventListener('click', function (event){
     const coordinateArray = event.target.id.split(',')
@@ -17,51 +34,98 @@ grid.addEventListener('click', function (event){
     const y = coordinateArray[1]
     gameState.board[x][y] = gameState.currentPlayer
     if (gameState.currentPlayer === 'X') {
+        event.target.innerText = 'X'
         gameState.currentPlayer = 'O'
     } else if (gameState.currentPlayer === 'O'){
+        event.target.innerText = 'O'
         gameState.currentPlayer = 'X'
     }
 })
 
+
 //**HEADER */
-let h1 = document.querySelector('h1')
-function makeh1(h1){
-h1.innerText('TIC TAC TOE')
+//**BUTTON TO CHOOSE 1P OR 2P? follow up on deselect when both are clicked*/
+let button1 = document.createElement('button');
+button1.innerText= "Single Player Game?";
+let h1tag = document.getElementsByTagName('h1')[0]
+let button2 = document.createElement('button');
+button2.innerText= "Two Player Game?";
+h1tag.after(button2);
+console.log(h1tag)
+h1tag.after(button1);
+
+let selectedColor = ".darkgrey";
+
+button1.addEventListener('click', function (event){
+    gameState.singlePlayer = false;
+    console.log(event.target.tagName)
+    if(event.target.tagName === 'BUTTON'){
+        event.target.classList.toggle('darkgrey');
+        console.log(event.target, 'color')
+        }
+    console.log(event.target, 'gameState changed')
+})
+
+button2.addEventListener('click', function (event){
+    gameState.singlePlayer = false;
+    console.log(event.target.tagName)
+    if(event.target.tagName === 'BUTTON'){
+        event.target.classList.toggle('darkgrey');
+        console.log(event.target, 'color')
+        }
+    console.log(event.target, 'gameState changed')
+})
+
+
+function colorize (event){
+    const target = event.target
+    
+        if(event.target.tagName === 'button'){
+        event.target.classList.toggle(selectedColor);
+        console.log(event.target, 'color')
+        }
+        console.log(event.target.tagName, 'outside if')
 }
-innerText.value = h1.innerText
-console.log(h1)
+
+//**USER NAME INPUT */
 
 
 //**SCORE TICKER */
+// should display player names and 'Computer' for 1p
 
-//**BUTTON TO CHOOSE 1P OR 2P? */
+
 
 //Render Function ==> Loop over the board, use the
 // indexes and values to query the element and 
 // update it in the browser
 
-//update progress
-function update(progress) {
-    gameState.board[x][y] += progress
-
-    if(gameState.board = finished
-    ){
-        gameState.board = finished
-    }
-}
+//** UPDATE PROGRESS, */
+// function update(progress) {
+//     gameState.board[x][y] += progress
+//     setInterval() => {
+//     if(gameState.board = finished
+//     ){
+//         //clear gameState.board
+//         //increment winnerscore(function needed)
+//     } else {
+//             //gameState.board = keep playing
+//     }
+//     }
+// }
 //alternative below
 // function gameLoop(){
 //     window.requestAnimationFrame(gameLoop);
 //     Game.update();
 //   }
 
-
-function updateScore() {
-    //update score ticker
-}
+//** UPDATE SCORE FUNCTION */
+// function updateScore() {
+//     //update score ticker
+// }
 
 
 //winning functions
 // check every possibility? no
 // any 3 X in a line?
 // or any 3 O in a line
+// 8 possible per player, easiest is an if else brute force
