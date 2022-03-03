6d8a0e99ec2b3f9a2cd850f21e63451b7b75ba5e
Stephen Lance McDaniel <stephen.l.mcdaniel@gmail.com>
2022-03-02 23:34:32 -0600

    End of night: added an alert and win checker

--- app.js
+++ app.js
@@ -18,22 +18,83 @@ const gameState = {
 };
 
 const grid = document.querySelector(".grid");
+
 //board checker needs to be inside grid listener
 grid.addEventListener("click", function (event) {
   const coordinateArray = event.target.id.split(",");
   console.log(coordinateArray); //remove this later
   const x = coordinateArray[0];
   const y = coordinateArray[1];
-  if (event.target != "X" || "O") {
+  let b1 = document.getElementById("0,0");
+  let b2 = document.getElementById("0,1");
+  let b3 = document.getElementById("0,2");
+  let b4 = document.getElementById("1,0");
+  let b5 = document.getElementById("1,1");
+  let b6 = document.getElementById("1,2");
+  let b7 = document.getElementById("2,0");
+  let b8 = document.getElementById("2,1");
+  let b9 = document.getElementById("2,2");
+
+  // not quite functioning *overwrite not present
+  if (event.target !== "X" || "O") {
     gameState.board[x][y] = gameState.currentPlayer;
     if (gameState.currentPlayer === "X") {
       event.target.innerText = "X";
       gameState.currentPlayer = "O";
+      if (gameState.board[0][0] === "X" && gameState.board[0][1] === "X" && gameState.board[0][2] === "X") {
+        window.alert("Player X won");
+        }
+        else if (gameState.board[0][0] === "X" && gameState.board[1][0] === "X" && gameState.board[2][0] === "X") {
+            window.alert("Player X won");
+            }
+        else if (gameState.board[2][0] === "X" && gameState.board[2][1] === "X" && gameState.board[2][2] === "X") {
+                window.alert("Player X won");
+                }
+        else if (gameState.board[0][2] === "X" && gameState.board[1][2] === "X" && gameState.board[2][2] === "X") {
+            window.alert("Player X won");
+            }
+        else if (gameState.board[0][0] === "X" && gameState.board[1][1] === "X" && gameState.board[2][2] === "X") {
+            window.alert("Player X won");
+            }
+        else if (gameState.board[0][2] === "X" && gameState.board[1][1] === "X" && gameState.board[2][0] === "X") {
+            window.alert("Player X won");
+            }
+        else if (gameState.board[0][1] === "X" && gameState.board[1][1] === "X" && gameState.board[2][1] === "X") {
+            window.alert("Player X won");
+            }
+        else if (gameState.board[1][0] === "X" && gameState.board[1][1] === "X" && gameState.board[1][2] === "X") {
+            window.alert("Player X won");
+            }
+        
     } else if (gameState.currentPlayer === "O") {
       event.target.innerText = "O";
       gameState.currentPlayer = "X";
-    }
-  }
+      if (gameState.board[0][0] === "O" && gameState.board[0][1] === "O" && gameState.board[0][2] === "O") {
+        window.alert("Player O won");
+        }
+        else if (gameState.board[0][0] === "O" && gameState.board[1][0] === "O" && gameState.board[2][0] === "O") {
+            window.alert("Player O won");
+            }
+        else if (gameState.board[2][0] === "O" && gameState.board[2][1] === "O" && gameState.board[2][2] === "O") {
+                window.alert("Player O won");
+                }
+        else if (gameState.board[0][2] === "O" && gameState.board[1][2] === "O" && gameState.board[2][2] === "O") {
+            window.alert("Player O won");
+            }
+        else if (gameState.board[0][0] === "O" && gameState.board[1][1] === "O" && gameState.board[2][2] === "O") {
+            window.alert("Player O won");
+            }
+        else if (gameState.board[0][2] === "O" && gameState.board[1][1] === "O" && gameState.board[2][0] === "O") {
+            window.alert("Player O won");
+            }
+        else if (gameState.board[0][1] === "O" && gameState.board[1][1] === "O" && gameState.board[2][1] === "O") {
+            window.alert("Player O won");
+            }
+        else if (gameState.board[1][0] === "O" && gameState.board[1][1] === "O" && gameState.board[1][2] === "O") {
+            window.alert("Player O won");
+            }
+    } 
+}
 });
 
 //**HEADER */
@@ -48,36 +109,34 @@ let nameForm = document.createElement("form");
 let nameInput = document.createElement("input");
 nameInput.setAttribute("type", "text");
 nameInput.setAttribute("name", "username");
-nameInput.setAttribute("id", "nameInput")
+nameInput.setAttribute("id", "nameInput");
 
 let nameSubmit = document.createElement("input");
 nameSubmit.setAttribute("type", "submit");
-nameSubmit.setAttribute("id", "nameSubmit")
+nameSubmit.setAttribute("id", "nameSubmit");
 // nameSubmit.setAttribute("value", "Submit");
 
 nameForm.appendChild(nameInput);
 nameForm.appendChild(nameSubmit);
 
-
-function getVal(){
-    let value = document.getElementById('nameInput');
-    console.log(value)
+function getVal() {
+  let value = document.getElementById("nameInput");
+  console.log(value);
 }
 getVal();
 
-
 // Submit button eventListener (maybe use a ternary)
-nameSubmit.addEventListener("click", function (event){
-    event.preventDefault()
+nameSubmit.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    if(gameState.playerOneName === ''){
-    gameState.playerOneName = nameInput.value
-    } else if(gameState.playerOneName !== ''){
-        gameState.playerTwoName = nameInput.value
-    }
-    console.log(gameState.playerOneName)
-    console.log(gameState.playerTwoName)
-})
+  if (gameState.playerOneName === "") {
+    gameState.playerOneName = nameInput.value;
+  } else if (gameState.playerOneName !== "") {
+    gameState.playerTwoName = nameInput.value;
+  }
+  console.log(gameState.playerOneName);
+  console.log(gameState.playerTwoName);
+});
 
 //**BUTTON TO CHOOSE 1P OR 2P? follow up on deselect when both are clicked*/
 //
@@ -101,13 +160,11 @@ h1tag.after(button2);
 h1tag.after(button1);
 
 // SCORE TICKER VARIABLES (i++ each score with each win)
-let score1 = document.getElementsByClassName('score1');
+let score1 = document.getElementsByClassName("score1");
 
-let score2 = document.getElementsByClassName('score2');
+let score2 = document.getElementsByClassName("score2");
 
-let scorePC = document.getElementsByClassName('scorePC');
-
-
+let scorePC = document.getElementsByClassName("scorePC");
 
 button1.addEventListener("click", function (event) {
   gameState.PlayerOrder();
